7ac69f5dfa9d43cd4b9281708657c4eb2357f4cd
GTrepublic <hans040515@gmail.com>
2019-07-25 13:08:44 +0800

    add symmetry tree

--- tree/Traverse(recursive)/101.对称二叉树(best_solution).cpp
+++ tree/Traverse(recursive)/101.对称二叉树(best_solution).cpp
@@ -1,3 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=cpp
+ *
+ * [94] 二叉树的中序遍历
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
